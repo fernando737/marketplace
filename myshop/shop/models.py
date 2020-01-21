10acb93ad2b58a9ce35cbde60c91e7509322af80
fernando737 <fernando.perezdp1@gmail.com>
2020-01-21 20:50:44 +0000

    Fix models.py ordering value must be a tuple. Add shop app to settings development and production

--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -6,7 +6,7 @@ class Category(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
 
     class Meta:
-        ordering = ('name')
+        ordering = ('name',)
         verbose_name = 'category'
         verbose_name_plural = 'categories'
 
@@ -20,12 +20,12 @@ class Product(models.Model):
     image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    available = models.BooleandField(default=True)
+    available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = ('name')
+        ordering = ('name',)
         index_together = (('id','slug'),)
     
     def __str__(self):
