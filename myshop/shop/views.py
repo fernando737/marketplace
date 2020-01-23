a0e833124b9bcbfba4cf84cdf04e9079345f2ff5
fernando737 <fernando.perezdp1@gmail.com>
2020-01-23 20:51:04 +0000

    Add values to urls.py models.py views.py and templates

--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -10,3 +10,7 @@ def product_list(request, category_slug=None):
         products = products.filter(Category=category)
     return render(request, 'shop/product/list.html',{'category': category, 'categories': categories, 'products': products})
 
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product, id=id, slug=slug, available=True)
+    return render(request, 'shop/product/detail.html', {'product':product})
+
